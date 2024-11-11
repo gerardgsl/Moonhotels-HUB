3d0b6da03c2d4b28beb58f27e1f3b21a391c9670
Gerard <gerardsoley1998@gmail.com>
2024-11-11 08:39:33 +0100

    swagger

--- MoonhotelsHUB/src/main/java/com/moonhotels/MoonhotelsHUB/adapter/in/web/controller/HubController.java
+++ MoonhotelsHUB/src/main/java/com/moonhotels/MoonhotelsHUB/adapter/in/web/controller/HubController.java
@@ -3,19 +3,38 @@ package com.moonhotels.MoonhotelsHUB.adapter.in.web.controller;
 import com.moonhotels.MoonhotelsHUB.domain.model.HubSearchRequest;
 import com.moonhotels.MoonhotelsHUB.domain.model.HubSearchResponse;
 import com.moonhotels.MoonhotelsHUB.domain.port.in.SearchHotelUseCase;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/hub")
 public class HubController {
 
-    private final SearchHotelUseCase searchHotelUseCase;
-
+    @Autowired
+    private SearchHotelUseCase searchHotelUseCase;
+    
+    @Autowired
     public HubController(SearchHotelUseCase searchHotelUseCase) {
         this.searchHotelUseCase = searchHotelUseCase;
     }
-
+    
+    @Operation(summary = "Search for available hotels")
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of hotels",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = HubSearchResponse.class)) }),
+        @ApiResponse(responseCode = "400", description = "Invalid search parameters",
+            content = @Content),        
+    })
     @PostMapping("/search")
     public HubSearchResponse searchHotels(@RequestBody HubSearchRequest request) {
         return searchHotelUseCase.searchHotels(request);
--- MoonhotelsHUB/src/main/java/com/moonhotels/MoonhotelsHUB/domain/model/HubSearchRequest.java
+++ MoonhotelsHUB/src/main/java/com/moonhotels/MoonhotelsHUB/domain/model/HubSearchRequest.java
@@ -2,6 +2,7 @@ package com.moonhotels.MoonhotelsHUB.domain.model;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,10 +11,16 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class HubSearchRequest {
+	@Schema(description = "ID of the hotel", example = "1")
     private int hotelId;
+	@Schema(description = "Check-in date", example = "2018-10-20")
     private LocalDate checkIn;
+	@Schema(description = "Check-out date", example = "2018-10-25")
     private LocalDate checkOut;
+	@Schema(description = "Number of guests", example = "3")
     private int numberOfGuests;
+	@Schema(description = "Number of rooms", example = "2")
     private int numberOfRooms;
+	@Schema(description = "Currency", example = "EUR")
     private String currency;   	
 }
